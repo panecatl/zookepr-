369f5164c6a15219c6cd6956a09727f379b432ac
panecatl <ypanecatl24@gmail.com>
2022-07-07 09:52:24 -0500

    added search parameters

--- server.js
+++ server.js
@@ -50,6 +50,11 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+// function that takes id and array, but returns a single animal object
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id) [0];
+    return result;
+}
 
 // route to animals.json
 app.get('/api/animals', (req, res) => {
@@ -58,6 +63,15 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
